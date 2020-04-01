9f619523c120640a34cf5f996c0fa6367375ea67
wanyong <wanyong@live.unc.edu>
2020-04-01 00:05:56 -0400

    connect to back-end by using axios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LeftSide from "./components/LeftSide"
 import RightSide from "./components/RightSide"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   let [points, setPoints] = useState(5);
   let [correct, setCorrect] = useState(false);
--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 let ex = "small → smallest \xa0\xa0 large → largest \xa0\xa0 fancy → fanciest";
-let fques = "The new comedy was one of the (funny) ";
-let sques = " movies I have ever seen.";
-let answer = "funniest";
 
-function GameBox({setCorrect, setSubmitted, submitted}) {
+function GameBox({ setCorrect, setSubmitted }) {
   let [value, setValue] = useState("");
+  let [answer, setAnswer] = useState("");
+  let [question, setQuestion] = useState("");
 
   useEffect(() => {
+    async function fetchAnswer() {
+      let response = await axios.get("http://localhost:5000/getgameanswer");
+      setAnswer(response.data);
+    }
+    async function fetchQuestion() {
+      let response = await axios.get("http://localhost:5000/getgamequestion");
+      setQuestion(response.data);
+    }
+    fetchAnswer();
+    fetchQuestion();
     setSubmitted(false);
-  }, [value]);
+  }, [value, answer, question]);
 
   let onChange = e => {
     setValue(e.target.value);
@@ -18,22 +28,35 @@ function GameBox({setCorrect, setSubmitted, submitted}) {
 
   let check = () => {
     setCorrect(answer === value);
-    setSubmitted(true)
-  }
+    setSubmitted(true);
+  };
+
+  let showQuestion = question => {
+    let result = question.split(")");
+    let firstPart = result[0] + ") ";
+    let secondPart = result[1] + " ";
+    return (
+      <div>
+        {firstPart} 
+        {<input type="text" value={value} onChange={onChange} />}
+        {secondPart}
+      </div>
+    );
+  };
   return (
     <div className="GameBox">
       <b>{ex}</b>
       <br />
       <br />
       <br />
-      {fques}
-      {<input type="text" value={value} onChange={onChange} />}
-      {sques}
+      {showQuestion(question)}
       <br />
       <br />
       <br />
       <br />
-      <button disabled={!value} onClick = {check}>submit</button>
+      <button disabled={!value} onClick={check}>
+        submit
+      </button>
     </div>
   );
 }
